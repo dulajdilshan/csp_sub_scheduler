97f4407a5d401ae32c537f141e2aac9b1e266909
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2018-06-05 09:49:22 +0530

    delete comments, creating arrays done

--- sched.py
+++ sched.py
@@ -1,30 +1,15 @@
 import csv
 
-#----------------------------------------------------------------#
-#       Defining input and output read/write functions           #
-#----------------------------------------------------------------#
+input_path = "input.csv"
+output_path = "output.csv"
 
-inputpath = "input.csv"
-outputpath = "output.csv"
-
-def get_in(input_path, inps):
-    input_file = open(input_path, 'rb')
-    lines = csv.reader(input_file, delimiter=' ')
-    for row in lines:
-        inps.append(row)
-    return inps
-
-def write_out(output_path, result):
-    output_file = open(output_path, 'wb')
-    lines = csv.writer(output_file, delimiter=',')
+def write_out(path, result):
+    out_file = open(path, 'wb')
+    w_lines = csv.writer(out_file, delimiter=',')
     for i in iter(result):
-        lines.writerow([i] + result[i])
+        w_lines.writerow([i] + result[i])
 
-#----------------------------------------------------------------#
-#                Creating arrays from the data                   #
-#----------------------------------------------------------------#
 
-inps = []
 subs = []
 types = []
 rec_slots = []
@@ -32,21 +17,22 @@ rooms = []
 
 ass_slots_rooms = []
 
-inps = get_in(inputpath,[])
-
-for i in range(len(inps)-1):
-    subs.append(inps[i][0].strip(','))
-    types.append(inps[i][1].strip(','))
-    slot = [j.strip(',') for j in inps[i][2:]]
+input_file = open(input_path, 'rb')
+lines = csv.reader(input_file, delimiter=' ')
+lines = list(lines)
+for row in lines[:-1]:
+    subs.append(row[0].strip(','))
+    types.append(row[1].strip(','))
+    slot = [j.strip(',') for j in row[2:]]
     rec_slots.append(slot)
-rooms = [i.strip(',') for i in inps[-1]]
+rooms = [el.strip(',') for el in lines[-1]]
 
 print subs
 print types
 print rec_slots
 print rooms
 
-#----------------------------------------------------------------#
-#                     Main Code                                  #
-#----------------------------------------------------------------#
+# Code of backtracking
 
+def is_safe():
+    return 
